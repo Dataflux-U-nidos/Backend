c4485cec02899372bb3b05174b71da0fab13d72e
Jose Rodriguez <93455373+Kose117@users.noreply.github.com>
2025-03-16 13:33:59 -0500

    Revert "86a77yfwu - Change jobs array to jobId reference in job opportunities"

--- src/application/use-cases/jobOpportunities/delete-jobOpportunities.use-case.ts
+++
@@ -1,9 +0,0 @@
-import { JobOpportunitiesRepository } from "../../../infrastructure/database/repositories/jobOpportunities.repository.impl";
-
-export class DeleteJobOpportunitiesUseCase {
-    constructor(private readonly jobOpportunitiesRepository: JobOpportunitiesRepository) {}
-  
-    public async execute(id: string): Promise<boolean> {
-      return this.jobOpportunitiesRepository.delete(id);
-    }
-  }
