e7d03a64a743b7fd51c3bc25567ec81265f8e507
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2025-03-26 21:01:51 -0500

    (86a7hqqyp) testing branch (#54)
    * testing branch
    
    * :memo: Update: reviewed pr comments

--- src/server.ts
+++ src/server.ts
@@ -14,13 +14,16 @@ import {
   authRouter,
 } from './presentation/routes';
 
-// Crear la aplicación Express
+// Create express application
 const app = express();
 
-// 1. Aplicar middlewares
+// Use Middlewares
 configureMiddlewares(app);
 
-// 2. Routes
+// Error handler
+app.use(errorHandlerMiddleware);
+
+// Routes
 app.use(`${config.api.conventionApi}/major`, majorRouter);
 app.use(`${config.api.conventionApi}/user`, userRouter);
 app.use(
@@ -31,18 +34,15 @@ app.use(`${config.api.conventionApi}/opportunity`, JobOpportunityRouter);
 app.use(`${config.api.conventionApi}/comment`, commentRouter);
 app.use(`${config.api.conventionApi}/auth`, authRouter);
 
-// Ruta de prueba
+// Testing routes
 app.get('/', (req, res) => {
   res.send('Servidor Express funcionando correctamente');
 });
 
-// 3. Middleware para manejo de errores
-app.use(errorHandlerMiddleware);
-
-// Conectar la base de datos antes de iniciar el servidor
+// Connect to database and start server
 const startServer = async () => {
   try {
-    await database.connect(); // Ensure DB is connected before starting the server
+    await database.connect();
     app.listen(config.server.port, () => {
       console.log(`🚀 Servidor corriendo en el puerto ${config.server.port}`);
     });
@@ -52,5 +52,5 @@ const startServer = async () => {
   }
 };
 
-// Iniciar la aplicación
+// Start server
 startServer();
