998285932ca6b1cbbd6cfe9e335edf01535ceb60
Juan Esteban Granada O'Brien <134427814+Juaness06@users.noreply.github.com>
2025-04-15 17:07:41 -0500

    ✅ test (86a7d5kgh) viewer login test and corrections of login admin test (#67)
    * ✅ test (86a7d5kgh) viewer login test and corrections of login admin test
    
    * 🐛 corrections for github actions
    
    * 🐛 Minor sintax fix
    
    * 🐛 fix env, config errors

--- src/infrastructure/config/config.ts
+++ src/infrastructure/config/config.ts
@@ -4,7 +4,7 @@ import { parseTime } from '../../shared/utils/time.utils';
 
 dotenv.config();
 
-const requiredEnvVars = ['MONGO_URI', 'CONVENTION_API', 'JWT_SECRET'];
+const requiredEnvVars = ['MONGO_URI', 'CONVENTION_API', 'JWT_SECRET', 'JWT_SECRET_REFRESH'];
 validateEnv(requiredEnvVars);
 
 const config = {
