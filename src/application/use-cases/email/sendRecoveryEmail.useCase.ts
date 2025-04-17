817e10f95b91941e29cf784b31fdd1d750e6f9e0
Juan Miguel Zuluaga Suárez <junzulu28@gmail.com>
2025-04-17 17:48:35 -0500

    ♻️ refactor: remove unnecessary console logs and improve method documentation in sendRecoveryEmail use case

--- src/application/use-cases/email/sendRecoveryEmail.useCase.ts
+++ src/application/use-cases/email/sendRecoveryEmail.useCase.ts
@@ -5,22 +5,22 @@ import config from '../../../infrastructure/config';
 export class SendRecoveryEmailUseCase {
   constructor(private readonly userRepository: IUserRepository) {}
 
+  // This method sends a recovery email to the user if the email exists in the database.
   public async execute(email: string): Promise<string> {
     const user = await this.userRepository.findByEmail(email);
     if (!user) {
       return 'Si el correo existe, recibirás instrucciones';
     }
 
-    console.log('SendRecoveryEmailUseCase - 1:', email);
+    // if user exists, send recovery email
     const payload = {
       to: email,
       subject: 'Recuperación de contraseña',
       type: 'PASSWORD_RECOVERY',
     };
 
+    // Call the lambda function to send the email
     const response = await axios.post(config.lambda.lambdaUrl, payload);
-
-    console.log('SendRecoveryEmailUseCase - 2:', email);
     return response.data;
   }
 }
